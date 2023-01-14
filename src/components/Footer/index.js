757108aeeba6f5118d2a6dc3f626997a80812c55
OtavioCanedo <ocneves@sga.pucminas.br>
2023-01-14 14:36:49 -0300

    feat: adição de ícones para os links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,19 @@
-import Link from '../links'
 import Heading from '../Heading'
+import { IconButton } from "@mui/material"
+import { FaGithub as Github } from 'react-icons/fa'
+import { AiOutlineMail as Email } from 'react-icons/ai'
+import { BsLinkedin as Linkedin } from 'react-icons/bs'
 
 import './styles.css'
 
 
 const Footer = () => (
     <footer className="footer">
-        <Heading>Read me Out</Heading>
+        <Heading lineBottom>Entre em contato</Heading>
         <div className="footer__link">
-            <Link link="mailto:otavionevescoc@hotmail.com" title="Email" />
-            <Link link="https://github.com/OtavioCanedo" title="Github" />
-            <Link link="https://www.instagram.com/otavio_canedo/" title="Instagram" />
-            <Link link="https://www.linkedin.com/in/ot%C3%A1vio-canedo-235267b3/" title="LinkedIn" />
+            <IconButton href="https://www.linkedin.com/in/otaviocanedo" target="_blank"><Linkedin size={40} color="#FF0A54" /></IconButton>
+            <IconButton href="https://github.com/OtavioCanedo" target="_blank"><Github size={40} color="#FF0A54" /></IconButton>
+            <IconButton href="mailto:otavionevescoc@hotmail.com" target="_blank"><Email size={40} color="#FF0A54" /></IconButton>
         </div>
     </footer>
 )

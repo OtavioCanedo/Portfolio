c3494911e81f21db079fa3b9c7e1527306f860a1
OtavioCanedo <ocneves@sga.pucminas.br>
2023-01-14 14:48:29 -0300

    feat: adição de novas habilidades

--- src/Pages/Home/skills.js
+++ src/Pages/Home/skills.js
@@ -1,35 +1,75 @@
-import { SiReact } from 'react-icons/si'
+import { SiReact as React } from 'react-icons/si'
 import { AiFillHtml5 as HTML } from 'react-icons/ai'
 import { DiCss3 as CSS } from 'react-icons/di'
 import { IoLogoJavascript as JS } from 'react-icons/io'
-import { SiC as C } from 'react-icons/si'
 import { SiJava as Java } from 'react-icons/si'
+import { SiMysql as Mysql } from 'react-icons/si'
+import { SiMaterialui as Materialui } from 'react-icons/si'
+import { FaGithub as Github } from 'react-icons/fa'
+import { ImGit as Git } from 'react-icons/im'
+import { DiNodejs as Nodejs } from 'react-icons/di'
+import { SiJirasoftware as Jira } from 'react-icons/si'
+import { SiNextdotjs as Nextjs } from 'react-icons/si'
+import { FaPython as Python } from 'react-icons/fa' 
+import { SiInsomnia as Insomnia } from 'react-icons/si'
 
-const skills = [
+const Skills = [
     {
-      image: SiReact,
-      title: 'React'
+      image: React,
+      title: 'React.js'
     },
     {
-      image: HTML,
-      title: 'HTML'
+      image: Nextjs,
+      title: 'Next.js'
     },
     {
       image: CSS,
-      title: 'CSS'
+      title: 'CSS3'
+    },
+    {
+      image: Materialui,
+      title: 'Material UI'
     },
     {
       image: JS,
       title: 'JavaScript'
     },
     {
-      image: C,
-      title: 'C Language'
+      image: HTML,
+      title: 'HTML5'
+    },
+    {
+      image: Git,
+      title: 'Git'
+    },
+    {
+      image: Github,
+      title: 'Github'
+    },
+    {
+      image: Nodejs,
+      title: 'Node.js'
+    },
+    {
+      image: Mysql,
+      title: 'MySQL'
     },
     {
       image: Java,
       title: 'Java'
+    },
+    {
+      image: Python,
+      title: 'Python'
+    },
+    {
+      image: Insomnia,
+      title: 'Insomnia'
+    },
+    {
+      image: Jira,
+      title: 'Jira (SCRUM)'
     }
   ]
 
-export default skills
+export default Skills

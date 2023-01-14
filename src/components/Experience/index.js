e2593787eec6752e115116fcdff534aecd084cdd
OtavioCanedo <ocneves@sga.pucminas.br>
2023-01-14 14:35:26 -0300

    feat: nova experiência profissional

---
+++ src/components/Experience/index.js
@@ -0,0 +1,18 @@
+import { Typography } from '@mui/material'
+
+const Experience = () => (
+    <Typography>
+        Atuei como estagiário em Desenvolvimento Web na empresa Easy Quant por 5 meses, e realizei as seguintes atividades:<br/><br/>   
+         • Criar novas interfaces no Front-end com Material UI e React, estilizadas com CSS3 e responsivas.<br/>
+         • Refatorar códigos existentes, utilizando versões mais recentes das tecnologias utilizadas.<br/>
+         • Fazer requisições a APIs, para exibir dados na tela ou autenticar o usuário, validando o Login ou cadastrando um novo.<br/>
+         • 1 projeto utilizando Next.js, 2 projetos usando ViteJs e 1 utilizando somente React.<br/>
+         • Fazer requisições REST no Insomnia<br/>
+         • Corrigir bugs, dando manutenção em funcionalidades já existentes<br/>
+         • Versionamento de código com Git e Github<br/>
+         • SCRUM com Jira<br/>
+         • A aplicação web foi escrita com arquitetura RESTful utilizando a stack MERN (Mongo DB, Express, React.js e Node). <br/>
+    </Typography>
+)
+
+export default Experience

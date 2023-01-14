a98c1ac41074c22b7c92f5388cd91d897b07d731
OtavioCanedo <ocneves@sga.pucminas.br>
2023-01-14 14:48:48 -0300

    feat: adição dos projetos realizados

--- src/Pages/Home/projects.js
+++ src/Pages/Home/projects.js
@@ -1,27 +1,18 @@
 const projects = [
     {
-        title: 'New project',
-        description: "Descrição do projeto"
+        title: 'Portfolio',
+        description: "Projeto que desenvolvi esse Portfolio, para divulgar outros projetos.",
+        link: 'https://github.com/OtavioCanedo/Portfolio'
     },
     {
-        title: 'New project',
-        description: "Descrição do projeto"
+        title: 'Calculadora',
+        description: "Projeto que desenvolvi uma calculadora, feita com tela de login.",
+        link: 'https://github.com/OtavioCanedo/Calculadora'
     },
     {
-        title: 'New project',
-        description: "Descrição do projeto"
-    },
-    {
-        title: 'New project',
-        description: "Descrição do projeto"
-    },
-    {
-        title: 'New project',
-        description: "Descrição do projeto"
-    },
-    {
-        title: 'New project',
-        description: "Descrição do projeto"
+        title: 'Imersão React Alura',
+        description: "Projeto de um chat que simula o discord, para conversar e acessar com o Github.",
+        link: 'https://github.com/OtavioCanedo/imersaoReactAlura'
     }
 ]
 
